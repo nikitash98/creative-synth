5cb7349bac2e67d07fdc8200fab35769c435b695
Nikita <nikitash98@gmail.com>
2017-12-27 23:45:48 -0500

    Should have been in the last commit.

--- src/DrawBuffer.java
+++ src/DrawBuffer.java
@@ -5,9 +5,16 @@ import javax.swing.JPanel;
 public class DrawBuffer extends JPanel {
 	private byte[] buffer = {0};
 	public void paintComponent(Graphics g){
-		for(int i = 0; i < buffer.length; i++){
+		super.paintComponent(g);
+		int themid = this.getSize().height/2;
+		g.drawLine(0, themid, this.getSize().width, themid);
+		
+		g.drawLine(0, themid + 127, this.getSize().width, themid + 127);
+		g.drawLine(0, themid - 127, this.getSize().width, themid - 127);
+
+		for(int i = 0; i < buffer.length && i < this.getSize().width; i++){
 			if(i > 0){
-				g.drawLine(i-1, 127 + buffer[i-1], i, 127 + buffer[i]);
+				g.drawLine(i-1, this.getSize().height/2 + buffer[i-1], i, this.getSize().height/2 + buffer[i]);
 			}
 		}
 		
